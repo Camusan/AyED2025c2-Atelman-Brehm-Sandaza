4a258438953f6b2e7d5bf6ab9d6d78d0bb20bac9
Camusan <casandaza@gmail.com>
2025-08-06 12:10:43 -0300

    Cambio 2

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,3 +1,4 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
 print("Hola desde el módulo 1")
+print("Este es el módulo 1 del proyecto")
