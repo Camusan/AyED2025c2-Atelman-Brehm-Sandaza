6229ec896b9c713fb3424571d0fb0ac64e0525a7
Camusan <casandaza@gmail.com>
2025-09-03 12:29:58 -0300

    Corrimos todos los test, no hay errores e hicimos las graficas

---
+++ TrabajoPractico_1/proyecto_1/modules/Graficacion.py
@@ -0,0 +1,50 @@
+from modules.Problema1 import ListaDobleEnlazada
+from matplotlib import pyplot as plt
+from random import randint
+import time
+
+elementos = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+tiempo_len = []
+tiempo_copiar= []
+tiempo_invertir= []
+for n in elementos:
+    lista= ListaDobleEnlazada()
+    for i in range(n):
+        dato=randint(1,100)
+        lista.agregar_al_final(dato)
+    contador=0
+    for i in range(n):
+        inicio=time.perf_counter()
+        lista.__len__()
+        fin=time.perf_counter()
+        contador=fin-inicio
+    tiempo_len.append(contador)
+    
+    contador=0
+    for i in range(n):
+        inicio=time.perf_counter()
+        lista.copiar()
+        fin=time.perf_counter()
+        contador=fin-inicio
+    tiempo_copiar.append(contador)
+
+    contador=0
+    for i in range(n):
+        inicio=time.perf_counter()
+        lista.invertir()
+        fin=time.perf_counter()
+        contador=fin-inicio
+    tiempo_invertir.append(contador)
+
+#Grafico de los tres metodos
+plt.figure(figsize=(10, 6))
+plt.plot(elementos, tiempo_len, marker='o', label='Tiempo de len()')
+plt.plot(elementos, tiempo_copiar, marker='o', label='Tiempo de copiar()')
+plt.plot(elementos, tiempo_invertir, marker='o', label='Tiempo de invertir()')
+plt.xlabel('Número de elementos en la lista')
+plt.ylabel('Tiempo en segundos)')
+plt.title('Comparación de cantidad de elpython modules\Graficacion.pyementos vs tiempo de ejecucion de los metodos len(), copiar() e invertir()')
+plt.legend()
+plt.grid()
+plt.show()    
+
--- TrabajoPractico_1/proyecto_1/modules/Problema1.py
+++ TrabajoPractico_1/proyecto_1/modules/Problema1.py
@@ -12,91 +12,94 @@ class ListaDobleEnlazada:
     def __init__(self):
         self.cabeza=None
         self.cola=None
-        self.tamaño=0
+        self.tamanio=0
         
     def esta_vacia(self):
-        return self.tamaño==0
+        return self.tamanio==0
 
     def agregar_al_inicio(self,item):
         nuevo_nodo= Nodo(item)
-        if self.tamaño==0:
+        if self.tamanio==0:
             self.cabeza=nuevo_nodo
             self.cola=nuevo_nodo
         else:
             nuevo_nodo.siguiente=self.cabeza
             self.cabeza.anterior=nuevo_nodo
             self.cabeza=nuevo_nodo
-        self.tamaño += 1
+        self.tamanio += 1
 
     def agregar_al_final(self,item):
         nuevo_nodo=Nodo(item)
-        if self.tamaño==0:
+        if self.tamanio==0:
             self.cabeza=nuevo_nodo
             self.cola=nuevo_nodo
         else:
             self.cola.siguiente=nuevo_nodo
             nuevo_nodo.anterior=self.cola
             self.cola=nuevo_nodo
-        self.tamaño+=1
-    
-    def insertar(self,dato,posicion=None):#Salio error en el test al insertar en el interior 
-        if posicion < 0 or posicion > self.tamaño:
+        self.tamanio+=1
+
+    def insertar(self,dato,posicion=None):#Salio error en el test al insertar en el interior
+        if posicion < 0 or posicion > self.tamanio:
             raise Exception("Posicion Invalida")
         if posicion == 0:
             self.agregar_al_inicio(dato)
-        elif posicion == self.tamaño:
+        elif posicion == self.tamanio:
             self.agregar_al_final(dato)
         else:
             nuevo_nodo = Nodo(dato)
             actual = self.cabeza
             for _ in range(posicion):
                 actual = actual.siguiente
-            nuevo_nodo.anterior = actual.anterior
+            
+            anterior = actual.anterior
+            nuevo_nodo.anterior = anterior
             nuevo_nodo.siguiente = actual
-            actual.anterior.siguiente = nuevo_nodo
+            anterior.siguiente = nuevo_nodo
             actual.anterior = nuevo_nodo
-            self.tamaño += 1
+            self.tamanio += 1
 
-    def extraer(self,posicion=None):#Salio error en el test 
-        if self.esta_vacia()==True:
+    def extraer(self, posicion=None):
+        if self.esta_vacia():
             raise Exception("Extraer de una lista vacia deberia arrojar un error")
         if posicion is None:
-            dato=self.cola.dato
-            if self.tamaño==1:
-                self.cabeza=None
-                self.cola=None
-            else:
-                self.cola=self.cola.anterior
-                self.cola.siguiente=None
-            self.tamaño-=1
-            return dato
-        if posicion < 0 :
-            raise Exception("Extraer de una posicion negativa debe arrojar error")
-        if posicion > self.tamaño - 1:
+            posicion = self.tamanio - 1  # Extraer el último
+        if posicion < 0:
+            if abs(posicion) > self.tamanio:
+                raise Exception("Extraer de una posicion fuera de rango debe arrojar error")
+            posicion = self.tamanio + posicion  # Soporta negativos como Python
+        if posicion >= self.tamanio or posicion < 0:
             raise Exception("Extraer de una posicion fuera de rango debe arrojar error")
+        # Extraer cabeza
         if posicion == 0:
-            dato=self.cabeza.dato
-            self.cabeza=self.cabeza.siguiente
-            self.cola=None 
-            self.tamaño-=1
+            dato = self.cabeza.dato
+            if self.tamanio == 1:
+                self.cabeza = None
+                self.cola = None
+            else:
+                self.cabeza = self.cabeza.siguiente
+                self.cabeza.anterior = None
+            self.tamanio -= 1
             return dato
-        if posicion == self.tamaño - 1:
+        # Extraer cola
+        if posicion == self.tamanio - 1:
             dato = self.cola.dato
-            if self.tamaño == 1:
+            if self.tamanio == 1:
                 self.cabeza = None
                 self.cola = None
             else:
                 self.cola = self.cola.anterior
                 self.cola.siguiente = None
-            self.tamaño -= 1
+            self.tamanio -= 1
             return dato
-        actual = self._obtener_nodo(posicion)
+        # Extraer interior
+        actual = self.cabeza
+        for _ in range(posicion):
+            actual = actual.siguiente
         dato = actual.dato
-
         actual.anterior.siguiente = actual.siguiente
         actual.siguiente.anterior = actual.anterior
-
-        self.tamaño -= 1
+        self.tamanio -= 1
         return dato
 
     def copiar(self):
@@ -107,35 +110,34 @@ class ListaDobleEnlazada:
             actual = actual.siguiente
         return copia
 
-    def invertir(self): #Fallo el test 
+    def invertir(self):
         if self.esta_vacia():
             return self
         actual = self.cabeza
+        self.cola = self.cabeza 
         while actual is not None:
             actual.siguiente, actual.anterior = actual.anterior, actual.siguiente
             if actual.anterior is None:
-                self.cabeza = actual
+                self.cabeza = actual 
             actual = actual.anterior
-        self.cola = self.cabeza
         return self
         
-        
     def concatenar(self, otra_lista):
-            if otra_lista.esta_vacia():
-                return self
-            copiar_otra = otra_lista.copiar()
-            if self.esta_vacia():
-                self.cabeza = copiar_otra.cabeza
-                self.cola = copiar_otra.cola
-            else:
-                self.cola.siguiente = copiar_otra.cabeza
-                copiar_otra.cabeza.anterior = self.cola
-                self.cola = copiar_otra.cola
-            self.tamaño += copiar_otra.tamaño
+        if otra_lista.esta_vacia():
             return self
+        copiar_otra = otra_lista.copiar()
+        if self.esta_vacia():
+            self.cabeza = copiar_otra.cabeza
+            self.cola = copiar_otra.cola
+        else:
+            self.cola.siguiente = copiar_otra.cabeza
+            copiar_otra.cabeza.anterior = self.cola
+            self.cola = copiar_otra.cola
+        self.tamanio += copiar_otra.tamanio
+        return self
         
     def __len__(self):
-        return self.tamaño
+        return self.tamanio
         
     def __add__(self, otra_lista):
         nueva_lista = self.copiar()
@@ -144,21 +146,17 @@ class ListaDobleEnlazada:
 
     def __iter__(self):
         actual = self.cabeza
-        for i in range(self.tamaño):
+        for i in range(self.tamanio):
             yield actual.dato
             actual = actual.siguiente
 
-
-
     def __str__(self):
-        # sirve para poder mostrar el contenido de una LDE por consola con la función print
         elementos = []
         actual = self.cabeza
         while actual is not None:
             elementos.append(str(actual.dato))
             actual = actual.siguiente
         return " <-> ".join(elementos)
-
 
 if __name__ == "__main__":
     lista=ListaDobleEnlazada()
