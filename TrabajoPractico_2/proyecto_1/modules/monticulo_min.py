92831c5953dbb932711ebdc601e31f5cf76d3553
n_nader <nadernadia@hotmail.com>
2025-09-29 16:12:40 -0300

    Ya creamos la clase paciente e implementamos el Monticulo de Minima

---
+++ TrabajoPractico_2/proyecto_1/modules/monticulo_min.py
@@ -0,0 +1,76 @@
+
+
+class Monticulo_Min:
+    def __init__(self):
+        self.lista_monticulo=[None]
+        self.tamaño_actual=0
+
+    def raiz(self):#Seria paciente con mayor criticidad
+        if self.tamaño_actual>=1:
+            return self.lista_monticulo[1]
+        else:
+            return None
+    
+    def hijo_min(self,i):
+        "Devuelve el hijo menor del monticulo de minimo segun el valor en la posicion i"
+        if i > len(self.lista_monticulo)-1:
+            raise IndexError("El indice esta fuera de rango")
+        if i * 2 + 1 > self.tamaño_actual:
+            return i * 2
+        else:
+            if self.lista_monticulo[i*2] < self.lista_monticulo[i*2+1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+
+    def infiltrar_abajo(self,i):
+        # se infiltra hacia abajo cuando el ancestro es mayor al hijo
+        while (i*2)<= self.tamaño_actual:
+            hijo_min= self.hijo_min(i)
+            if self.lista_monticulo[i] > self.lista_monticulo[hijo_min]:
+                tmp = self.lista_monticulo[i]
+                self.lista_monticulo[i] = self.lista_monticulo[hijo_min]
+                self.lista_monticulo[hijo_min] = tmp
+            i = hijo_min
+
+    def infiltrar_arriba(self,i):
+        # se infiltra hacia arriba cuando el hijo es menor al ancestro
+        while i // 2 > 0:
+            if self.lista_monticulo[i] < self.lista_monticulo[i // 2]:
+                tmp = self.lista_monticulo[i // 2]
+                self.lista_monticulo[i // 2] = self.lista_monticulo[i]
+                self.lista_monticulo[i] = tmp
+            i = i // 2
+
+    def sacar_raiz(self):#Se atiende paciente con mayor criticidad
+        '''Saca la raiz de un arbol y toma el ultimo valor agrega y lo pone en la raiz
+        y va infiltrando hacia abajo si es necesario'''
+        valorSacado = self.lista_monticulo[1]
+        self.lista_monticulo[1] = self.lista_monticulo[self.tamaño_actual]
+        self.tamaño_actual = self.tamaño_actual - 1
+        self.lista_monticulo.pop()
+        self.infiltrar_abajo(1)
+        return valorSacado
+    
+    def insertar_valor(self, valor):#LLega un nuevo paciente 
+        self.lista_monticulo.append(valor)
+        self.tamaño_actual += 1
+        self.infiltrar_arriba(self.tamaño_actual)
+        self.infiltrar_abajo(self.tamaño_actual)
+
+
+if __name__ == "__main__":
+    monticulo = Monticulo_Min()
+    monticulo.insertar_valor(5)
+    monticulo.insertar_valor(3)
+    monticulo.insertar_valor(8)
+    monticulo.insertar_valor(1)
+    monticulo.insertar_valor(4)
+
+    print("Raíz del montículo:", monticulo.raiz())  # Debería imprimir 1
+
+    print("Sacando la raíz:", monticulo.sacar_raiz())  # Debería imprimir 1
+    print("Nueva raíz del montículo:", monticulo.raiz())  # Debería imprimir 3
+
+    print("Sacando la raíz:", monticulo.sacar_raiz())  # Debería imprimir 3
+    print("Nueva raíz del montículo:", monticulo.raiz())  # Debería imprimir 4
---
+++ TrabajoPractico_2/proyecto_1/modules/paciente.py
@@ -0,0 +1,5 @@
+
+import random 
+class Paciente:
+    def __init__(self):
+        self.criticidad=random.randint(1,3)
