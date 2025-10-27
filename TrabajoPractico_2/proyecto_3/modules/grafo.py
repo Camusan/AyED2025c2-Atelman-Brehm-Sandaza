0781b308df2d0609e8fc1d9807c9af88c8d0d857
n_nader <nadernadia@hotmail.com>
2025-10-26 23:12:29 -0300

    Cambios lindos

--- TrabajoPractico_2/proyecto_3/modules/grafo.py
+++ TrabajoPractico_2/proyecto_3/modules/grafo.py
@@ -1,18 +1,37 @@
+from modules.vertice import Vertice
 class Grafo:
     def __init__(self):
-        self.listaVertices = {}
+        self.listaVertices = {} #
         self.numVertices = 0
 
     def agregarVertice(self, clave):
-        pass
+        "agrega una instancia de vertice al grafo "
+        if clave in self.listaVertices:
+            raise KeyError("El vértice ya existe en el grafo")
+        vertice=Vertice(clave)
+        self.listaVertices[clave]=vertice
+        self.numVertices += 1
+        return vertice
+        
 
     def obtenerVertice(self, n):
-        pass
+        " encuentra el vértice en el grafo con nombre n."
+        if n in self.listaVertices:
+            return self.listaVertices[n]
+        else:
+            return None
 
     def agregarArista(self, de, a, costo=0):
-        pass
+        "agrega al grafo una nueva arista dirigida que conecta dos vértices."
+        if de not in self.listaVertices:
+            nv = self.agregarVertice(de)
+        if a not in self.listaVertices:
+            nv = self.agregarVertice(a)
+        self.listaVertices[de].agregarVecino(self.listaVertices[a], costo)
+
 
     def obtenerVertices(self):
+        "devuelve la lista de todos los vértices en el grafo."
         return self.listaVertices.keys()
 
     def __iter__(self):
