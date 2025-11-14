31e13348045e99d776816279c92c7081937e2b17
Camusan <casandaza@gmail.com>
2025-11-14 16:24:21 -0300

    Cambios en el monticulo

--- TrabajoPractico_2/proyecto_1/modules/monticulo_min.py
+++ TrabajoPractico_2/proyecto_1/modules/monticulo_min.py
@@ -30,7 +30,8 @@ class Monticulo_Min:
         self.lista_monticulo.append(valor)
         self.tamaño_actual += 1
         self.infiltrar_arriba(self.tamaño_actual)
-        
+        self.infiltrar_abajo(self.tamaño_actual)
+
 
 
     def hijo_min(self, i):
@@ -48,7 +49,20 @@ class Monticulo_Min:
                 return i * 2 + 1
 
     
+    def infiltrar_abajo(self, i):
+        # se infiltra hacia abajo cuando el ancestro es mayor al hijo
+        while (i*2) <= self.tamaño_actual:
+            hijo_min = self.hijo_min(i)
+            actual = self.lista_monticulo[i]
+            hijo = self.lista_monticulo[hijo_min]
+            if (actual.riesgo, -actual.prioridad) > (hijo.riesgo, -hijo.prioridad):
+                self.lista_monticulo[i], self.lista_monticulo[hijo_min] = hijo, actual
+            i = hijo_min
+
+
+
     def infiltrar_arriba(self, i):
+        # se infiltra hacia arriba cuando el hijo es menor al ancestro
         # se infiltra hacia arriba cuando el hijo es menor al ancestro
         while i // 2 > 0:
             actual = self.lista_monticulo[i]
@@ -60,7 +74,7 @@ class Monticulo_Min:
     
 
 
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     monticulo = Monticulo_Min()
     pac1=Paciente()
     print(pac1.criticidad)
@@ -68,7 +82,7 @@ if __name__ == "__main__":
     print(pac2.criticidad)
     monticulo.insertar_valor(pac1.criticidad)
     monticulo.insertar_valor(pac2.criticidad)
-    print("Raíz del montículo:", monticulo.raiz())  # Debería imprimir la criticidad menor
+    print("Raíz del montículo:", monticulo.raiz())'''  # Debería imprimir la criticidad menor
     # monticulo.insertar_valor(5)
     # monticulo.insertar_valor(3)
     # monticulo.insertar_valor(8)
@@ -82,3 +96,23 @@ if __name__ == "__main__":
 
     # print("Sacando la raíz:", monticulo.sacar_raiz())  # Debería imprimir 3
     # print("Nueva raíz del montículo:", monticulo.raiz())  # Debería imprimir 4
+
+if __name__ == "__main__":
+        monticulo = Monticulo_Min()
+        pac1=Paciente()
+        print(pac1.riesgo, pac1.prioridad)
+        pac2=Paciente()
+        print(pac2.riesgo, pac2.prioridad)
+        monticulo.insertar_valor(pac1)
+        monticulo.insertar_valor(pac2)     
+        # print("Raíz del montículo:", monticulo.raiz())  # Debería imprimir la criticidad menor
+        # monticulo.insertar_valor(5)
+        # monticulo.insertar_valor(3)       
+        #   print("Sacando la raíz:", monticulo.sacar_raiz())
+        #  print("Nueva raíz del montículo:", monticulo.raiz())
+        # print("Sacando la raíz:", monticulo.sacar_raiz())
+        # print("Nueva raíz del montículo:", monticulo.raiz())
+
+
+
+
